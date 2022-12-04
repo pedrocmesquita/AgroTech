1cef4a8c75deba869859eb808596eaaad92909b9
goncrib <1210792@isep.ipp.pt>
2022-12-04 20:12:26 +0000

    teste3/4 us301

--- Tests/US/US301Test.java
+++ Tests/US/US301Test.java
@@ -13,6 +13,7 @@ import org.junit.jupiter.api.BeforeEach;
 import java.io.File;
 import java.io.FileNotFoundException;
 
+import static Shared.constants.Files.*;
 import static org.junit.Assert.assertEquals;
 
 
@@ -21,22 +22,14 @@ public class US301Test {
     CsvReader readFiles;
     Graph<Local,Integer> map=null;
     File file2,file1;
-    @BeforeEach
-    void before(){
-        readFiles=new CsvReader();
-        file1=new File("C:\\Users\\pnsri\\OneDrive\\Ambiente de Trabalho\\Nova pasta\\Sem3pi\\src\\FICHEIROS_LEITURA\\Small\\distancias_small.csv");
-        file2=new File("C:\\Users\\pnsri\\OneDrive\\Ambiente de Trabalho\\Nova pasta\\Sem3pi\\src\\FICHEIROS_LEITURA\\Small\\clientes-produtores_small.csv");
 
-        map=new MapGraph<>(false);
-
-    }
 
     @Test
     void teste1() throws FileNotFoundException {
 
         readFiles=new CsvReader();
-        file1=new File("C:\\Users\\pnsri\\OneDrive\\Ambiente de Trabalho\\Nova pasta\\Sem3pi\\src\\FICHEIROS_LEITURA\\Small\\distancias_small.csv");
-        file2=new File("C:\\Users\\pnsri\\OneDrive\\Ambiente de Trabalho\\Nova pasta\\Sem3pi\\src\\FICHEIROS_LEITURA\\Small\\clientes-produtores_small.csv");
+        file1=new File(s_distancias);
+        file2=new File(s_clientes_produtores);
 
         map=new MapGraph<>(false);
 
@@ -52,8 +45,8 @@ public class US301Test {
     void teste2() throws FileNotFoundException {
 
         readFiles=new CsvReader();
-        file1=new File("C:\\Users\\pnsri\\OneDrive\\Ambiente de Trabalho\\Nova pasta\\Sem3pi\\src\\FICHEIROS_LEITURA\\Small\\distancias_small.csv");
-        file2=new File("C:\\Users\\pnsri\\OneDrive\\Ambiente de Trabalho\\Nova pasta\\Sem3pi\\src\\FICHEIROS_LEITURA\\Small\\clientes-produtores_small.csv");
+        file1=new File(s_distancias);
+        file2=new File(s_clientes_produtores);
 
         map=new MapGraph<>(false);
 
@@ -61,7 +54,41 @@ public class US301Test {
         BST<Destinatário> destinatários=readFiles.getDestinatários();//arvore dos destinatários
         readFiles.ReadDistancias(file1,file2,",",map,locais);//grafo (vertices-locais)
 
-        assertEquals(map.numEdges(),33);
+        assertEquals(map.numEdges(),66);
+
+
+    }
+
+    void teste3() throws FileNotFoundException {
+
+        readFiles=new CsvReader();
+        file1=new File(b_distancias);
+        file2=new File(b_clientes_produtores);
+
+        map=new MapGraph<>(false);
+
+        BST<Local> locais=readFiles.ReadClientesProdutores(file2,",");//arvore dos locais
+        BST<Destinatário> destinatários=readFiles.getDestinatários();//arvore dos destinatários
+        readFiles.ReadDistancias(file1,file2,",",map,locais);//grafo (vertices-locais)
+
+        assertEquals(map.numVertices(),323);
+
+
+    }
+
+    void teste4() throws FileNotFoundException {
+
+        readFiles=new CsvReader();
+        file1=new File(b_distancias);
+        file2=new File(b_clientes_produtores);
+
+        map=new MapGraph<>(false);
+
+        BST<Local> locais=readFiles.ReadClientesProdutores(file2,",");//arvore dos locais
+        BST<Destinatário> destinatários=readFiles.getDestinatários();//arvore dos destinatários
+        readFiles.ReadDistancias(file1,file2,",",map,locais);//grafo (vertices-locais)
+
+        assertEquals(map.numEdges(),1566);
 
 
     }
