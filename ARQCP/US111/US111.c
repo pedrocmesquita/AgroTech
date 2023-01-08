c3fb46a70a08bfe54c5142b087e3013450459a94
mig567 <miguelpessmarques@gmail.com>
2023-01-08 23:58:26 +0000

    Small edit on main.

--- ARQCP/US111/US111.c
+++ ARQCP/US111/US111.c
@@ -1,10 +1,18 @@
-int add_sensor(sensor s, sensor* array, int size)
+int add_sensor(Sensor *s, Sensor *array, int size)
 {
-array = realloc(array, sizeof(sensor)*size);
-array[size-1]=s;
+	array = (Sensor*) realloc(array, sizeof(sensor)*size);
+	array[size-1] = *s;
+	return size++;
 }
 
-int remove_sensor(sensor s, sensor* array, int size)
+int remove_sensor(Sensor *array, int size)
 {
-array = realloc(array, sizeof(sensor)*size);
+	array = (Sensor*) realloc(array, sizeof(sensor)*size);
+	return size--;
+}
+
+void alt_freq(long new_freq, Sensor *s)
+{
+	*s.frequency = new_freq;
+	*s.readings_size = 86400 / new_freq;
 }
